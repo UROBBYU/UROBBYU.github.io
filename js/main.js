43b1dfb476b8bde70a07af90c658a835a3209816
UROBBYU <srobby228@gmail.com>
2019-10-03 01:22:32 +0300

    Test 2

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ document.getElementsByClassName("icon wip")[0].classList = "icon wip" + ((docume
 document.getElementsByClassName("icon idea")[0].classList = "icon idea" + ((document.getElementsByClassName("icon idea")[0].classList.length == 3) ? "" : " move");
 document.getElementsByClassName("icon profile")[0].classList = "icon profile" + ((document.getElementsByClassName("icon profile")[0].classList.length == 3) ? "" : " move");
 }
+
+function move2() {
+var mv = ((document.getElementsByClassName("center-button links")[0].style.transform == "rotate(0deg)") ? 1 : 0);
+document.getElementsByClassName("center-button links")[0].style.transform = "rotate(" + 60*mv + "deg)";
+document.getElementsByClassName("icon links")[0].style.transform = "rotate(-" + 60*mv + "deg)";
+document.getElementsByClassName("center-button download")[0].style.transform = "rotate(" + 120*mv + "deg)";
+document.getElementsByClassName("icon download")[0].style.transform = "rotate(-" + 120*mv + "deg)";
+document.getElementsByClassName("center-button wip")[0].style.transform = "rotate(" + 180*mv + "deg)";
+document.getElementsByClassName("icon wip")[0].style.transform = "rotate(-" + 180*mv + "deg)";
+document.getElementsByClassName("center-button idea")[0].style.transform = "rotate(" + 240*mv + "deg)";
+document.getElementsByClassName("icon idea")[0].style.transform = "rotate(-" + 240*mv + "deg)";
+document.getElementsByClassName("center-button profile")[0].style.transform = "rotate(" + 300*mv + "deg)";
+document.getElementsByClassName("icon profile")[0].style.transform = "rotate(-" + 300*mv + "deg)";
+}
