ba50bb19f34acbde1f72b2a4e95dc70f5e579600
UROBBYU <srobby228@gmail.com>
2019-10-03 19:08:16 +0300

    Test 3

--- js/main.js
+++ js/main.js
@@ -7,16 +7,11 @@ document.getElementsByClassName("icon idea")[0].classList = "icon idea" + ((docu
 document.getElementsByClassName("icon profile")[0].classList = "icon profile" + ((document.getElementsByClassName("icon profile")[0].classList.length == 3) ? "" : " move");
 }
 
-function move2() {
-var mv = ((document.getElementsByClassName("center-button links")[0].style.transform == "rotate(0deg)") ? 1 : 0);
-document.getElementsByClassName("center-button links")[0].style.transform = "rotate(" + 60*mv + "deg)";
-document.getElementsByClassName("icon links")[0].style.transform = "rotate(-" + 60*mv + "deg)";
-document.getElementsByClassName("center-button download")[0].style.transform = "rotate(" + 120*mv + "deg)";
-document.getElementsByClassName("icon download")[0].style.transform = "rotate(-" + 120*mv + "deg)";
-document.getElementsByClassName("center-button wip")[0].style.transform = "rotate(" + 180*mv + "deg)";
-document.getElementsByClassName("icon wip")[0].style.transform = "rotate(-" + 180*mv + "deg)";
-document.getElementsByClassName("center-button idea")[0].style.transform = "rotate(" + 240*mv + "deg)";
-document.getElementsByClassName("icon idea")[0].style.transform = "rotate(-" + 240*mv + "deg)";
-document.getElementsByClassName("center-button profile")[0].style.transform = "rotate(" + 300*mv + "deg)";
-document.getElementsByClassName("icon profile")[0].style.transform = "rotate(-" + 300*mv + "deg)";
+function move2(cls) {
+var mv = (document.getElementsByClassName("center-button profile")[0].style.transform != "rotate(300deg)" || document.getElementsByClassName("center-button goal")[0].style.transform != "rotate(0deg)");
+for (var i = 0; i < 6; i++) {
+  document.getElementsByClassName("center-button")[i].style["z-index"] = ((cls / 60 - 6 + i) < 0) ? (cls / 60 + i) : (cls / 60 - 6 + i);
+  document.getElementsByClassName("center-button")[5 - i].style.transform = "rotate(" + ((mv) ? (60*i) : ((cls > (60*i)) ? cls - 360 : cls)) + "deg)";
+  document.getElementsByClassName("icon")[5 - i].style.transform = "rotate(" + ((mv) ? (60*i) : ((cls > (60*i)) ? cls - 360 : cls))*(-1) + "deg)";
+}
 }
