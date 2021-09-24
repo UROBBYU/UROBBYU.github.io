c45cc4d7c3cea044463492726ca15e0e0e8f008d
UROBBYU <srobby228@gmail.com>
2021-09-24 22:48:09 +0300

    sw fix

--- sw.js
+++ sw.js
@@ -1,42 +1,48 @@
-const cacheVersion = 'v0.2'
-const contentToCache = [
-    '/',
-    '/css/style.css',
-    '/img/download.svg',
-    '/img/goal.svg',
-    '/img/home.svg',
-    '/img/idea.svg',
-    '/img/links.svg',
-    '/img/menu.svg',
-    '/img/profile.svg',
-    '/img/sprite.svg',
-    '/img/wip.svg',
-    '/js/main.js',
-    '/favicon.ico',
-    '/index.html',
-    '/Minesweeper/',
-    '/Minesweeper/img/apple-touch-icon.png',
-    '/Minesweeper/img/icon-192.png',
-    '/Minesweeper/img/icon-512.png',
-    '/Minesweeper/img/mstile-150.png',
-    '/Minesweeper/img/safari-pinned-tab.svg',
-    '/Minesweeper/img/browserconfig.xml',
-    '/Minesweeper/index.html',
-    '/Minesweeper/main.js',
-    '/Minesweeper/minesweeper.webmanifest',
-    '/Minesweeper/style.css'
-]
-const cacheToKeep = ['v0.2']
+const cacheData = {
+    main: [
+        '/',
+        '/css/style.css',
+        '/img/download.svg',
+        '/img/goal.svg',
+        '/img/home.svg',
+        '/img/idea.svg',
+        '/img/links.svg',
+        '/img/menu.svg',
+        '/img/profile.svg',
+        '/img/sprite.svg',
+        '/img/wip.svg',
+        '/js/main.js',
+        '/favicon.ico',
+        '/index.html'
+    ],
+    minesweeper: [
+        '/Minesweeper/',
+        '/Minesweeper/img/apple-touch-icon.png',
+        '/Minesweeper/img/icon-192.png',
+        '/Minesweeper/img/icon-512.png',
+        '/Minesweeper/img/mstile-150.png',
+        '/Minesweeper/img/safari-pinned-tab.svg',
+        '/Minesweeper/browserconfig.xml',
+        '/Minesweeper/index.html',
+        '/Minesweeper/main.js',
+        '/Minesweeper/minesweeper.webmanifest',
+        '/Minesweeper/style.css'
+    ]
+}
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheVersion).then(cache => cache.addAll(contentToCache)))
+    e.waitUntil(Promise.all(Object.keys(cacheData).map(name => 
+        caches.open(name).then(cache => 
+            cache.addAll(cacheData[name])
+        )
+    )))
 })
 
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(kL => Promise.all(kL.map(k => {
-        if (!cacheToKeep.includes(k)) return caches.delete(k)
-    }))))
-})
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(names => 
+    Promise.all(names.map(name => {
+        if (!Object.keys(cacheData).includes(name)) return caches.delete(name)
+    }))
+)))
 
 self.addEventListener('fetch', e => {
     e.respondWith((async () => {
