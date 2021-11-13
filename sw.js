2d21e9d1e0088568668cce6064a2ae4056651c86
UROBBYU <srobby228@gmail.com>
2021-11-13 20:56:43 +0200

    Versions update

--- sw.js
+++ sw.js
@@ -1,104 +1,200 @@
-const cacheData = {
-    v1: [
-        // Main
-        '/',
-        '/css/style.css',
-        '/img/administrator.svg',
-        '/img/download.svg',
-        '/img/Ghost Logo.svg',
-        '/img/goal.svg',
-        '/img/home.svg',
-        '/img/idea.svg',
-        '/img/links.svg',
-        '/img/menu.svg',
-        '/img/moderator.svg',
-        '/img/mstile-150.png',
-        '/img/premium.svg',
-        '/img/profile.svg',
-        '/img/sprite.svg',
-        '/img/wip.svg',
-        '/js/main.js',
-        '/browserconfig.xml',
-        '/favicon.ico',
-        '/index.html',
-        // Minesweeper
-        '/minesweeper/',
-        '/minesweeper/img/apple-touch-icon.png',
-        '/minesweeper/img/icon-192.png',
-        '/minesweeper/img/icon-512.png',
-        '/minesweeper/img/safari-pinned-tab.svg',
-        '/minesweeper/index.html',
-        '/minesweeper/main.js',
-        '/minesweeper/minesweeper.webmanifest',
-        '/minesweeper/style.css',
-        // Bookreader
-        '/bookreader/',
-        '/bookreader/custom-icons.woff',
-        '/bookreader/index.html',
-        '/bookreader/main.js',
-        '/bookreader/style.css',
-        'https://fonts.googleapis.com',
-        'https://fonts.gstatic.com',
-        'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'
-    ]
+const delayTime = 2000
+
+String.prototype.ver = function () {
+	return +this.substr(1)
 }
-const cacheKeys = Object.keys(cacheData)
-const curVer = cacheKeys.find(v => /^v\d+?$/.test(v))
 
-self.addEventListener('install', e => {
-    e.waitUntil(new Promise(async res => {
-        let lastVer = (await caches.keys()).find(v => /^v\d+?$/.test(v))
-        lastVer = (lastVer ? lastVer : 'v0')
-        console.log('lastVer', lastVer)
-        console.log('curVer', curVer)
-        if (lastVer < curVer) {
-            console.log('creating new version')
-            const cache = await caches.open(lastVer)
-            const newCache = await caches.open(curVer)
-            const promises = []
-            for (const key of await cache.keys()) {
-                if (cacheKeys.filter(v => v <= lastVer).find(v => (key in cacheData(v)))) {
-                    promises.push(newCache.put(key, await cache.match(key)))
-                }
-            }
-            const newVer = cacheKeys.filter(v => v > lastVer)
-            console.log('new versions', newVer)
-            for (const cacheKey of newVer) {
-                console.log('adding', cacheData[cacheKey])
-                promises.push(newCache.addAll(cacheData[cacheKey]))
-            }
-            console.log('installation is done')
-            await Promise.all(promises)
-        }
-        console.log('its over')
-        res()
-    }))
+const cacheData = {
+	v1: [
+		// Main
+		'/',
+		'/css/style.css',
+		'/img/administrator.svg',
+		'/img/download.svg',
+		'/img/Ghost Logo.svg',
+		'/img/goal.svg',
+		'/img/home.svg',
+		'/img/idea.svg',
+		'/img/links.svg',
+		'/img/menu.svg',
+		'/img/moderator.svg',
+		'/img/mstile-150.png',
+		'/img/premium.svg',
+		'/img/profile.svg',
+		'/img/sprite.svg',
+		'/img/wip.svg',
+		'/js/main.js',
+		'/browserconfig.xml',
+		'/favicon.ico',
+		'/index.html',
+		// Minesweeper
+		'/minesweeper/',
+		'/minesweeper/img/apple-touch-icon.png',
+		'/minesweeper/img/icon-192.png',
+		'/minesweeper/img/icon-512.png',
+		'/minesweeper/img/safari-pinned-tab.svg',
+		'/minesweeper/index.html',
+		'/minesweeper/main.js',
+		'/minesweeper/style.css',
+		// Bookreader
+		'/bookreader/custom-icons.woff',
+		'/bookreader/style.css',
+		'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
+	],
+	v2: [
+		'/minesweeper/minesweeper.webmanifest',
+		'/bookreader/',
+		'/bookreader/index.html',
+		'/bookreader/main.js',
+		'/bookreader/catalog.js',
+		'/bookreader/title.js',
+		'/bookreader/book.js',
+	],
+}
+const domain = 'https://urepo.online'
+const cacheKeys = Object.keys(cacheData).sort((v1, v2) => v1.ver() > v2.ver())
+const curVer = cacheKeys[cacheKeys.length - 1] ?? 'v0'
+
+const urlsToStore = [
+	'https://fonts.gstatic.com/',
+	'https://server.urepo.com.ua:8443/books/image/',
+]
+
+const needToStore = (url) => urlsToStore.find((v) => url.startsWith(v))
+
+self.addEventListener('install', (e) => {
+	e.waitUntil(
+		(async () => {
+			console.log('Installation started')
+			let lastVer = (await caches.keys()).reduce(
+				(t, v) => (/v\d+?/.test(v) && v.ver() > t.ver() ? v : t),
+				'v0'
+			)
+			if (lastVer.ver() < curVer.ver()) {
+				const oldCache = await caches.open(lastVer)
+				const newCache = await caches.open(curVer)
+				const promises = []
+				const controller = new AbortController()
+				for (const key of cacheKeys) {
+					console.log('key', key, key != curVer)
+					if (key != curVer) {
+						for (const url of cacheData[key]) {
+							console.log('url', url)
+							const cache = await oldCache.match(url)
+							if (cache) {
+								console.log('transfer')
+								promises.push(newCache.put(url, cache))
+							} else {
+								console.log('add')
+								promises.push(
+									(async () => {
+										try {
+											const res = await fetch(url, {
+												signal: controller.signal,
+											})
+											if (res.ok)
+												await newCache.put(url, res)
+										} catch (err) {}
+									})()
+								)
+							}
+						}
+					} else {
+						for (const url of cacheData[key]) {
+							console.log('url', url)
+							console.log('add')
+							promises.push(
+								(async () => {
+									try {
+										const res = await fetch(url, {
+											signal: controller.signal,
+										})
+										if (res.ok) await newCache.put(url, res)
+									} catch (err) {}
+								})()
+							)
+						}
+					}
+				}
+				const timerId = setTimeout(controller.abort, delayTime)
+				await Promise.all(promises)
+				clearTimeout(timerId)
+			}
+			console.log('Installation finished')
+		})()
+	)
 })
 
-self.addEventListener('activate', e => {
-    e.waitUntil(new Promise(async res => {
-        const promises = []
+self.addEventListener('activate', (e) => {
+	e.waitUntil(
+		(async () => {
+			const promises = []
 
-        for (const name of (await caches.keys()).filter(v => /^v\d+?$/.test(v) && v != curVer)) {
-            promises.push(caches.delete(name))
-        }
-        promises.push(caches.delete('main'))
-        promises.push(caches.delete('minesweeper'))
+			for (const key of await caches.keys()) {
+				if (key != curVer) promises.push(caches.delete(key))
+			}
 
-        Promise.all(promises).then(() => res())
-    }))
+			await Promise.all(promises)
+		})()
+	)
 })
 
-self.addEventListener('fetch', e => {
-    e.respondWith((async () => {
-        try {
-            const cache = await caches.match(e.request)
-            console.log('fetch', cache)
-            if (cache) return cache
-            return await fetch(e.request)
-        } catch (err) {
-            console.error(err)
-        }
-        return new Response(null, { status: 400 })
-    })())
+self.addEventListener('fetch', async (e) => {
+	if (e.request.method.toUpperCase() != 'GET') return
+
+	e.respondWith(
+		(async () => {
+			console.log('fetching', e.request)
+			let ret
+			const controller = new AbortController()
+			const timerId = setTimeout(() => controller.abort(), delayTime)
+
+			try {
+				let url = e.request.url
+				if (e.request.url.startsWith(domain + '/bookreader/?'))
+					url = domain + '/bookreader/'
+				const cache = await caches.match(url)
+				if (cache) ret = cache
+				else if (needToStore(url)) {
+					const newCache = await caches.open(curVer)
+					const req = new Request(url, {
+						mode:
+							e.request.mode == 'navigate'
+								? 'cors'
+								: e.request.mode,
+						credentials: e.request.credentials,
+						headers: e.request.headers,
+						signal: controller.signal,
+					})
+					console.log('req', req)
+					const res = await fetch(req)
+					console.log('data', res)
+					if (!res.ok) throw new Error('Network response was not OK')
+					newCache.put(url, res.clone())
+					ret = res
+				} else {
+					ret = await fetch(url, {
+						mode:
+							e.request.mode == 'navigate'
+								? 'cors'
+								: e.request.mode,
+						credentials: e.request.credentials,
+						headers: e.request.headers,
+						signal: controller.signal,
+					})
+				}
+			} catch (err) {
+				console.warn(err)
+			}
+
+			clearTimeout(timerId)
+			if (!ret)
+				ret = new Response(null, {
+					status: 400,
+					statusText: 'No response',
+				})
+			console.log('response', ret)
+			return ret
+		})()
+	)
 })
